7122aac587110ed475b9dfe73965e6db18a6dbf2
Thomas Rory Gummerson <thomas@gummerson.no>
2019-10-14 15:59:00 +0200

    Add sports enum

--- scrape/getEvents.js
+++ scrape/getEvents.js
@@ -6,10 +6,12 @@ const qs = require('querystring');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const getEvents = async (sportId = 1, date = new Date(), offset = 0) => {
+const sports = require('./sports');
+
+const getEvents = async (sport = sports.football, date = new Date(), offset = 0) => {
 	const url = 'http://www.elcomparador.com/html/contenido/mas_partidos.php?' +
 		qs.stringify({
-			deporte: sportId,
+			deporte: sport,
 			fecha: date.toISOString().split('T')[0],
 			offset
 		});
---
+++ scrape/sports.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const sports = {
+	football: 1,
+	volleyball: 2,
+	tennis: 23
+};
+
+module.exports = sports;
