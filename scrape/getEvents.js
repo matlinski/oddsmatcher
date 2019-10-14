ef9871a7b1566d19651f03d16f641ed394e455f6
Thomas Rory Gummerson <thomas@gummerson.no>
2019-10-14 15:47:40 +0200

    Find URLs in HTML source

--- scrape/getEvents.js
+++ scrape/getEvents.js
@@ -1,19 +1,24 @@
 'use strict';
 
+const { URL } = require('url');
 const qs = require('querystring');
 
 const axios = require('axios');
 const cheerio = require('cheerio');
 
 const getEvents = async (sportId = 1, date = new Date(), offset = 0) => {
-	const { data } = await axios(
-		'http://www.elcomparador.com/html/contenido/mas_partidos.php?' +
+	const url = 'http://www.elcomparador.com/html/contenido/mas_partidos.php?' +
 		qs.stringify({
 			deporte: sportId,
 			fecha: date.toISOString().split('T')[0],
 			offset
-		}));
-	return data;
+		});
+	const { data } = await axios(url);
+	const $ = cheerio.load(data);
+	return $('.flecha_izquierda')
+		.toArray()
+		.map(el => $(el).attr('href'))
+		.map(u => new URL(u, url).href);
 };
 
 module.exports = getEvents;
