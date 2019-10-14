e51e9fd8081645f912de9d3448a52255bade14ff
Thomas Rory Gummerson <thomas@gummerson.no>
2019-10-14 15:34:43 +0200

    use querystring

--- scrape/getEvents.js
+++ scrape/getEvents.js
@@ -1,14 +1,18 @@
 'use strict';
 
+const qs = require('querystring');
+
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const getEvents = async (sportId = 1, date = new Date()) => {
+const getEvents = async (sportId = 1, date = new Date(), offset = 0) => {
 	const { data } = await axios(
-		'http://www.elcomparador.com/html/contenido/mas_partidos.php?deporte=' +
-		sportId +
-		'&fecha=' +
-		date.toISOString().split('T')[0]);
+		'http://www.elcomparador.com/html/contenido/mas_partidos.php?' +
+		qs.stringify({
+			deporte: sportId,
+			fecha: date.toISOString().split('T')[0],
+			offset
+		}));
 	return data;
 };
 
