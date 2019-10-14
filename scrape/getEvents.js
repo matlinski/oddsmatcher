e7c9e74830587da38c95f5d2713c786aab5220f9
Thomas Rory Gummerson <thomas@gummerson.no>
2019-10-14 15:31:19 +0200

    getEvents basic arguments

--- scrape/getEvents.js
+++ scrape/getEvents.js
@@ -3,8 +3,13 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const getEvents = async () => {
-
+const getEvents = async (sportId = 1, date = new Date()) => {
+	const { data } = await axios(
+		'http://www.elcomparador.com/html/contenido/mas_partidos.php?deporte=' +
+		sportId +
+		'&fecha=' +
+		date.toISOString().split('T')[0]);
+	return data;
 };
 
 module.exports = getEvents;
