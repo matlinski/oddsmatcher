528fac3d81deefde889b9a7413c77bea1f32db74
Thomas Rory Gummerson <thomas@gummerson.no>
2019-10-15 09:36:12 +0200

    More odds data

--- scrape/index.js
+++ scrape/index.js
@@ -7,8 +7,8 @@ const getOdds = require('./getOdds');
 (async () => {
 
 	const events = await getEvents(sports.football);
-
+	console.log(events[0]);
 	const odds = await getOdds(events[0]);
 
-	console.log(odds);
+	// console.log(odds);
 })();
